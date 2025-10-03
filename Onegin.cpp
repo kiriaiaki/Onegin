d927369c15568b6dbc92a63d1a09f4d02759ff90
kiriaiaki <kirkos29028@gmail.com>
2025-10-03 22:37:37 +0300

    Add files via upload

--- Onegin.cpp
+++ Onegin.cpp
@@ -13,47 +13,45 @@ struct Data_Str
 
 int Strcmp_K (const Data_Str Data_1, const Data_Str Data_2);
 int Strcmp_Reverse_K (const Data_Str Data_1, const Data_Str Data_2);
-int Creation_Array_Struct_Based_Buffer (char* const Buffer, struct Data_Str* const Array_Struct,  const size_t Quantity_Struct);
-int Creation_Buffer_Based_On_Array_Struct (char* const Buffer, const struct Data_Str* const Array_Struct, const size_t Quantity_Struct);
+int Filling_Array_Struct_Based_Buffer (char* const Buffer, struct Data_Str* const Array_Struct,  const size_t Quantity_Struct);
+int Filling_Buffer_Based_On_Array_Struct (char* const Buffer, const struct Data_Str* const Array_Struct, const size_t Quantity_Struct);
 int Bubble_Sort_Array_Struct (struct Data_Str* const Array_Struct, const size_t Quantity_Struct, int (*Compare_Function) (const Data_Str Data_1, const Data_Str Data_2));
 int Counter_Symbol_In_Str (const char* const Str, const char Symbol);
-//TODO check stat and read
+int Copying_File_In_Buffer (char** Buffer, size_t* Size_Onegin);
+int Creation_Array_Struct_Based_On_Line (char** Buffer, struct Data_Str** Array_Struct, size_t* Quantity_Line_Onegin);
+int Open_File_And_Array_Struct (char** Buffer, size_t* Size_Onegin, struct Data_Str** Array_Struct, size_t* Quantity_Line_Onegin);
+int Print_Array_Struct_In_File (const struct Data_Str* const Array_Struct, const size_t Quantity_Struct, FILE* const File);
+
 int main ()
 {
-    struct stat Data_Mini_Onegin;
-    stat ("Mini_Onegin.txt", &Data_Mini_Onegin); //why & ?????
-    const size_t Size_Mini_Onegin = Data_Mini_Onegin.st_size;
+    struct Data_Str* Array_Struct = (struct Data_Str*) malloc (2008);
+    char* Buffer = (char*) malloc (2902);
+    size_t Quantity_Line_Onegin = 0;
+    size_t Size_Onegin = 0;
 
-    const int File_Mini_Onegin = open ("Mini_Onegin.txt", O_RDONLY);
-    char* Buffer = (char*) calloc (Size_Mini_Onegin, sizeof (char));
-    read (File_Mini_Onegin, Buffer, Size_Mini_Onegin);
-    close (File_Mini_Onegin);
-
-    const size_t Quantity_Line_Mini_Onegin = size_t (Counter_Symbol_In_Str (Buffer, '\n'));
-
-    struct Data_Str* Array_Struct = (struct Data_Str*) (calloc (Quantity_Line_Mini_Onegin, sizeof (Data_Str)));
-    Creation_Array_Struct_Based_Buffer (Buffer, Array_Struct, Quantity_Line_Mini_Onegin);
+    if (Open_File_And_Array_Struct (&Buffer, &Size_Onegin, &Array_Struct, &Quantity_Line_Onegin) != 0)
+    {
+        return 0;
+    }
 
     const int File_Answer = open ("Answer.txt", O_WRONLY);
+    FILE* File_Answer_F = fopen ("Answer_F.txt", "w");
 
-    Bubble_Sort_Array_Struct (Array_Struct, Quantity_Line_Mini_Onegin, Strcmp_K);
-    char* Printer_Buffer_1 = (char*) calloc (Size_Mini_Onegin, sizeof (char));
-    Creation_Buffer_Based_On_Array_Struct (Printer_Buffer_1, Array_Struct, Quantity_Line_Mini_Onegin);
-    write (File_Answer, Printer_Buffer_1, Size_Mini_Onegin);
+    Bubble_Sort_Array_Struct (Array_Struct, Quantity_Line_Onegin, Strcmp_K);
+    char* Printer_Buffer_1 = (char*) calloc (Size_Onegin, sizeof (char));
+    Filling_Buffer_Based_On_Array_Struct (Printer_Buffer_1, Array_Struct, Quantity_Line_Onegin);
+    write (File_Answer, Printer_Buffer_1, Size_Onegin);
     write (File_Answer, "\n\n\n", 3);
     free (Printer_Buffer_1);
 
-    Bubble_Sort_Array_Struct (Array_Struct, Quantity_Line_Mini_Onegin, Strcmp_Reverse_K);
-    char* Printer_Buffer_2 = (char*) calloc (Size_Mini_Onegin, sizeof (char));
-    Creation_Buffer_Based_On_Array_Struct (Printer_Buffer_2, Array_Struct, Quantity_Line_Mini_Onegin);
-    write (File_Answer, Printer_Buffer_2, Size_Mini_Onegin);
-    write (File_Answer, "\n\n\n", 3);
-    free (Printer_Buffer_2);
+    Bubble_Sort_Array_Struct (Array_Struct, Quantity_Line_Onegin, Strcmp_Reverse_K);
+    Print_Array_Struct_In_File (Array_Struct, Quantity_Line_Onegin, File_Answer_F);
 
-    write (File_Answer, Buffer, Size_Mini_Onegin);
+    write (File_Answer, Buffer, Size_Onegin);
     free (Buffer);
 
     close (File_Answer);
+    fclose (File_Answer_F);
 
     return 0;
 }
@@ -77,7 +75,7 @@ int Counter_Symbol_In_Str (const char* const Str, const char Symbol)
     return Counter;
 }
 
-int Creation_Array_Struct_Based_Buffer (char* const Buffer, struct Data_Str* const Array_Struct,  const size_t Quantity_Struct)
+int Filling_Array_Struct_Based_Buffer (char* const Buffer, struct Data_Str* const Array_Struct,  const size_t Quantity_Struct)
 {
     assert (Buffer != NULL);
     assert (Array_Struct != NULL);
@@ -129,9 +127,10 @@ int Bubble_Sort_Array_Struct (struct Data_Str* const Array_Struct, const size_t 
 
 int Strcmp_Reverse_K (const Data_Str Data_1, const Data_Str Data_2)
 {
+    size_t j = Data_1.Len_Str - 1, k = Data_2.Len_Str - 1;
+
     for (size_t i = 0; i < Data_1.Len_Str - 1; i++)
     {
-        size_t j = Data_1.Len_Str - 1, k = Data_2.Len_Str - 1;
         int Offset_1 = 0;
         int Offset_2 = 0;
 
@@ -173,7 +172,7 @@ int Strcmp_Reverse_K (const Data_Str Data_1, const Data_Str Data_2)
     return 0;
 }
 
-int Creation_Buffer_Based_On_Array_Struct (char* const Buffer, const struct Data_Str* const Array_Struct, const size_t Quantity_Struct)
+int Filling_Buffer_Based_On_Array_Struct (char* const Buffer, const struct Data_Str* const Array_Struct, const size_t Quantity_Struct)
 {
     assert (Buffer != NULL);
     assert (Array_Struct != NULL);
@@ -195,9 +194,9 @@ int Creation_Buffer_Based_On_Array_Struct (char* const Buffer, const struct Data
 
 int Strcmp_K (const Data_Str Data_1, const Data_Str Data_2)
 {
+    size_t j = 0, k = 0;
     for (size_t i = 0; i < Data_1.Len_Str; i++)
     {
-        size_t j = 0, k = 0;
         int Offset_1 = 0;
         int Offset_2 = 0;
 
@@ -235,6 +234,76 @@ int Strcmp_K (const Data_Str Data_1, const Data_Str Data_2)
         j++;
         k++;
     }
+
+    return 0;
+}
+
+int Copying_File_In_Buffer (char** Buffer, size_t* Size_Onegin)
+{
+    struct stat Data_Onegin = {};
+
+    if (stat ("Onegin.txt", &Data_Onegin) != 0)
+    {
+        return -1;
+    }
+
+    *Size_Onegin = Data_Onegin.st_size;
+
+    const int File_Onegin = open ("Onegin.txt", O_RDONLY);
+
+    *Buffer = (char*) calloc (*Size_Onegin, sizeof (char));
+
+    if (read (File_Onegin, *Buffer, *Size_Onegin) < *Size_Onegin)
+    {
+        return -1;
+    }
+
+    close (File_Onegin);
+
+    return 0;
+}
+
+int Creation_Array_Struct_Based_On_Line (char** Buffer, struct Data_Str** Array_Struct, size_t* Quantity_Line_Onegin)
+{
+    *Quantity_Line_Onegin = size_t (Counter_Symbol_In_Str (*Buffer, '\n'));
+
+    *Array_Struct = (struct Data_Str*) (calloc (*Quantity_Line_Onegin, sizeof (Data_Str)));
+    Filling_Array_Struct_Based_Buffer (*Buffer, *Array_Struct, *Quantity_Line_Onegin);
+
+    return 0;
+}
+
+int Open_File_And_Array_Struct (char** Buffer, size_t* Size_Onegin, struct Data_Str** Array_Struct, size_t* Quantity_Line_Onegin)
+{
+    if (Copying_File_In_Buffer (Buffer, Size_Onegin) != 0)
+    {
+        return -1;
+    }
+
+    Creation_Array_Struct_Based_On_Line (Buffer, Array_Struct, Quantity_Line_Onegin);
+
+    return 0;
+}
+
+int Print_Array_Struct_In_File (const struct Data_Str* const Array_Struct, const size_t Quantity_Struct, FILE* const File)
+{
+    assert (Array_Struct != NULL);
+
+    size_t k = 0;
+
+    for (size_t i = 0; i < Quantity_Struct; i++)
+    {
+        for (size_t j = 0; j < Array_Struct[i].Len_Str; j++)
+        {
+            fputc (Array_Struct[i].Ptr_Str[j], File);
+
+            k++;
+        }
+    }
+
+    fputc ('\n', File);
+    fputc ('\n', File);
+    fputc ('\n', File);
 
     return 0;
 }
